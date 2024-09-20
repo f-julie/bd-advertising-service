61990340374e996d52ad25b5f42f708db61a3ecb
Julie Fiess <juliefiess@gmail.com>
2024-09-20 14:14:23 -0500

    Final submission

--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -16,7 +16,7 @@ import java.util.concurrent.Future;
  */
 public class TargetingEvaluator {
     public static final boolean IMPLEMENTED_STREAMS = true;
-    public static final boolean IMPLEMENTED_CONCURRENCY = false;
+    public static final boolean IMPLEMENTED_CONCURRENCY = true;
     private final RequestContext requestContext;
 
     /**
